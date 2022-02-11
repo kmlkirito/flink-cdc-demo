f4a3f02b76d67524cd89fdce64595215e7fbb947
阚梦龙 <709628161@qq.com>
2022-02-11 18:10:04 +0800

    init

---
+++ src/main/java/com/kirito/MySqlSourceExample.java
@@ -0,0 +1,33 @@
+package com.kirito;
+
+import com.ververica.cdc.connectors.mysql.source.MySqlSource;
+import com.ververica.cdc.debezium.JsonDebeziumDeserializationSchema;
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+public class MySqlSourceExample {
+    public static void main(String[] args) throws Exception {
+        MySqlSource<String> mySqlSource = MySqlSource.<String>builder()
+                .hostname("119.45.20.118")
+                .port(3306)
+                .databaseList("xjd") // set captured database
+                .tableList("xjd.backend_user") // set captured table
+                .username("root")
+                .password("kirito1993~!@#")
+                .deserializer(new JsonDebeziumDeserializationSchema()) // converts SourceRecord to JSON String
+                .build();
+
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        // enable checkpoint
+        env.enableCheckpointing(3000);
+
+        env
+                .fromSource(mySqlSource, WatermarkStrategy.noWatermarks(), "MySQL Source")
+                // set 4 parallel source tasks
+                .setParallelism(4)
+                .print().setParallelism(1); // use parallelism 1 for sink to keep message ordering
+
+        env.execute("Print MySQL Snapshot + Binlog");
+    }
+}
