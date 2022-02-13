934207df697e70a1fcd52af8ef1285a52f929c3c
kanml <709628161@qq.com>
2022-02-14 07:27:51 +0800

    dataStream api demo

--- src/main/java/com/kirito/MySqlSourceExample.java
+++ src/main/java/com/kirito/MySqlSourceExample.java
@@ -1,6 +1,7 @@
 package com.kirito;
 
 import com.ververica.cdc.connectors.mysql.source.MySqlSource;
+import com.ververica.cdc.connectors.mysql.table.StartupOptions;
 import com.ververica.cdc.debezium.JsonDebeziumDeserializationSchema;
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
@@ -8,13 +9,14 @@ import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 public class MySqlSourceExample {
     public static void main(String[] args) throws Exception {
         MySqlSource<String> mySqlSource = MySqlSource.<String>builder()
-                .hostname("119.45.20.118")
+                .hostname("119.45.20.168")
                 .port(3306)
                 .databaseList("xjd") // set captured database
-                .tableList("xjd.backend_user") // set captured table
+                .tableList("xjd.flink_test") // set captured table
                 .username("root")
                 .password("kirito1993~!@#")
                 .deserializer(new JsonDebeziumDeserializationSchema()) // converts SourceRecord to JSON String
+                .startupOptions(StartupOptions.initial())
                 .build();
 
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
@@ -26,7 +28,8 @@ public class MySqlSourceExample {
                 .fromSource(mySqlSource, WatermarkStrategy.noWatermarks(), "MySQL Source")
                 // set 4 parallel source tasks
                 .setParallelism(4)
-                .print().setParallelism(1); // use parallelism 1 for sink to keep message ordering
+                .print()
+                .setParallelism(1); // use parallelism 1 for sink to keep message ordering
 
         env.execute("Print MySQL Snapshot + Binlog");
     }
